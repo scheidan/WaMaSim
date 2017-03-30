5b5c8446faa516c564a8f7ce41c0e0fb5fbb19bf
Andreas Scheidegger <andreas.scheidegger@eawag.ch>
2017-03-30 11:38:28 +0200

    update example

--- R/main.r
+++ R/main.r
@@ -11,7 +11,7 @@
 NULL
 
 ##' WaMaSim - Water Management Simulator
-##' 
+##'
 ##' WaMaSim is a package to simulate the effect of different
 ##' rehabiliation strategies for water distribution systems. It is an education tool
 ##' used for the Water Infrastructure Experimental and Computer Laboratory at ETH Zurich, Switzerland.
@@ -34,9 +34,9 @@ NULL
 ##'
 ##' The cost are calcualted as a finction of the diameter, assuming all pipes have a
 ##' length of 100 meters.
-##' 
+##'
 ##' @title Run a simulation
-##' 
+##'
 ##' @param t.sim number of years to simulate
 ##' @param expansion either a scalar the describing the number of pipes added
 ##' every year, or a vector of length \code{t.sim}.
@@ -48,126 +48,124 @@ NULL
 ##' @param initial.inventory If equals \code{NULL} the simulation starts without pipes,
 ##' if \code{initial.inventory} is integer it specifies the number of initial pipes,
 ##' or it can be a \code{data.frame} containing the initial inventory.
-##' 
+##'
 ##' @return A list of length \code{t.sim+1} containing all modeled states. A state
 ##' is a list consisting of the time, the budget, and the inventory at a given point in time.
 ##'
 ##' @seealso Different replacement strategies are implemented: \code{\link{replace.n.highest.risk}},
 ##' \code{\link{replace.n.oldest}}, \code{\link{replace.n.random}}, \code{\link{replace.older.than}},
 ##' \code{\link{replace.more.failures.than}}, \code{\link{do.nothing}}
-##' 
-##' @examples
 ##'
-##' ## Define failure rate
+##' @examples
+##
+##' # Define failure rate
 ##' f.rate <- function(age, time.last.failure, n.failure) {
-  ##'   if(n.failure==0){
-  ##'     return(1/30)
-  ##'   } else {
-  ##'     return(1/10)
-  ##'   }
+##'   if(n.failure==0){
+##'     return(1/30)
+##'   } else {
+##'     return(1/10)
+##'   }
 ##' }
-##' 
-##' 
-##' ## define a complicated (and probably useless) rehabilitation strategy
+##'
+##'
+##' # define a complicated (and probably useless) rehabilitation strategy
 ##' mystrategy <- . %>%
 ##'   replace.n.highest.risk(n=2, failure.rate=f.rate) %>%
 ##'   replace.more.failures.than(max.failures=5) %>%
-##'   replace.older.than(max.age=100) %>%
-##'   replace.n.oldest(n=3) %>%
-##'   replace.n.random(n=4)
-##' ## This means: every year (if we have enough budget!), replace first the 2 pipes
-##' ## with the highest risk, then all pipes with more than 5 failures,
-##' ## then all pipes older then 100 years, then the 3 oldest pipes remaining, and
-##' ## finally replace 4 randomly selected pipes. 
-##' 
-##' 
-##' ## run the simulation
+##'   replace.older.than(max.age=100, max.cost=2e6) %>%
+##'   replace.n.oldest(n=2) %>%
+##'   replace.n.random(n=2)
+##' # This means: every year (if we have enough budget!), replace first the 2 pipes
+##' # with the highest risk, then all pipes with more than 5 failures,
+##' # then all pipes older then 100 years, then the 3 oldest pipes remaining, and
+##' # finally replace 4 randomly selected pipes.
+##'
+##'
+##' # run the simulation
 ##' result <- simulate(t.sim=100,                  # run it for 100 years
-                       ##'                    expansion=0,                # do not expand the system
-                       ##'                    rehabilitation=mystrategy,  # use the strategy defined above
-                       ##'                    failure.rate=f.rate,        # use the failure rate defined above
-                       ##'                    income = 1e6,               # the annual income
-                       ##'                    initial.budget=30e6,   
-                       ##'                    initial.inventory=50)       # start the simulation with 50 new pipes
-                       ##' 
-                       ##' @author Andreas Scheidegger
-                       ##' @export
-                       simulate <- function(t.sim, 
-                                            expansion,   
-                                            rehabilitation,
-                                            failure.rate,
-                                            income=0,
-                                            initial.budget=Inf,
-                                            initial.inventory=NULL) {
+##'                    expansion=0,                # do not expand the system
+##'                    rehabilitation=mystrategy,  # use the strategy defined above
+##'                    failure.rate=f.rate,        # use the failure rate defined above
+##'                    income = 1e6,               # the annual income
+##'                    initial.budget=30e6,
+##'                    initial.inventory=500)      # start the simulation with 50 new pipes
+##'
+##' @author Andreas Scheidegger
+##' @export
+simulate <- function(t.sim,
+                     expansion,
+                     rehabilitation,
+                     failure.rate,
+                     income=0,
+                     initial.budget=Inf,
+                     initial.inventory=NULL) {
 
-                         if(!("function" %in% class(rehabilitation))){
-                           stop("argument 'rehabilitation' must be a function!")
-                         }
-                         if(!("function" %in% class(failure.rate))){
-                           stop("argument 'failure.rate' must be a function!")
-                         }
+  if(!("function" %in% class(rehabilitation))){
+    stop("argument 'rehabilitation' must be a function!")
+  }
+  if(!("function" %in% class(failure.rate))){
+    stop("argument 'failure.rate' must be a function!")
+  }
 
-                         if(length(expansion)==1) expansion <- rep(expansion, t.sim)
-                         if(length(income)==1) income <- rep(income, t.sim)
-  
-  
-                         ## build initial state
-                         if(!is.null(initial.inventory) &
-                            !is.numeric(initial.inventory) &
-                            !(class(initial.inventory)=="data.frame")) {
-                           stop("Argument 'initial.inventory' must be NULL, an integer or a data.frame!")
-                         }
-                         if(is.null(initial.inventory)){
-                           state <- list(inventory=make.empty.inventory(), budget=initial.budget, time=0)
-                         }
-                         if(is.numeric(initial.inventory)){
-                           state <- list(inventory=make.empty.inventory(), budget=initial.budget, time=0)
-                           state <- expand(state, initial.inventory, separat.budget=TRUE)
-                         }
-                         if(class(initial.inventory)=="data.frame"){
-                           if(colnames(initial.inventory) != c("ID","time.construction", "replacement.value",
-                                                               "diameter", "n.failure", "time.last.failure",
-                                                               "time.end.of.service", "in.service")){
-                             stop("incorrect column names of 'initial.inventory'!")
-                           }
-                           class(initial.inventory) <- c("data.frame", "inventory")
-                           state <- list(inventory=initial.inventory, budget=initial.budget, time=0)
-                         }
+  if(length(expansion)==1) expansion <- rep(expansion, t.sim)
+  if(length(income)==1) income <- rep(income, t.sim)
 
 
+  ## build initial state
+  if(!is.null(initial.inventory) &
+     !is.numeric(initial.inventory) &
+     !(class(initial.inventory)=="data.frame")) {
+    stop("Argument 'initial.inventory' must be NULL, an integer or a data.frame!")
+  }
+  if(is.null(initial.inventory)){
+    state <- list(inventory=make.empty.inventory(), budget=initial.budget, time=0)
+  }
+  if(is.numeric(initial.inventory)){
+    state <- list(inventory=make.empty.inventory(), budget=initial.budget, time=0)
+    state <- expand(state, initial.inventory, separat.budget=TRUE)
+  }
+  if(class(initial.inventory)=="data.frame"){
+    if(colnames(initial.inventory) != c("ID","time.construction", "replacement.value",
+                                        "diameter", "n.failure", "time.last.failure",
+                                        "time.end.of.service", "in.service")){
+      stop("incorrect column names of 'initial.inventory'!")
+    }
+    class(initial.inventory) <- c("data.frame", "inventory")
+    state <- list(inventory=initial.inventory, budget=initial.budget, time=0)
+  }
 
 
-                         ## list to keep results
-                         result <- vector("list", t.sim+1) 
-                         names(result) <- paste0("time.", 1:(t.sim+1))
-                         result[[1]] <- state
-  
-                         ## loop over time
-                         for(t in 1:t.sim){
-                           if(t%%10==0){
-                             print(paste("Simulate year", t))
-                           }
-    
-                           ## 0) update time
-                           state$time = state$time + 1
-    
-                           ## 1) expand system
-                           state <- expand(state, expansion[t],
-                                           separat.budget=TRUE) 
-    
-                           ## 2) collect fees
-                           state$budget <- state$budget + income[t]
-    
-                           ## 3) simulate failures
-                           state <- fail(state, failure.rate)
-
-                           ## ## 4) rehabilitate pipes
-                           state <- rehabilitation(state)
-
-                           result[[t+1]] <- state    
-                         }
-
-                         return(result)
-                       }
 
 
+  ## list to keep results
+  result <- vector("list", t.sim+1)
+  names(result) <- paste0("time.", 1:(t.sim+1))
+  result[[1]] <- state
+
+  ## loop over time
+  for(t in 1:t.sim){
+    if(t%%10==0){
+      print(paste("Simulate year", t))
+    }
+
+    ## 0) update time
+    state$time = state$time + 1
+
+    ## 1) expand system
+    state <- expand(state, expansion[t],
+                    separat.budget=TRUE)
+
+    ## 2) collect fees
+    state$budget <- state$budget + income[t]
+
+    ## 3) simulate failures
+    state <- fail(state, failure.rate)
+
+    ## # 4) rehabilitate pipes
+    state <- rehabilitation(state)
+
+    result[[t+1]] <- state
+  }
+
+  return(result)
+}
--- README.md
+++ README.md
@@ -39,27 +39,27 @@ f.rate <- function(age, time.last.failure, n.failure) {
 }
 
 
-## 2) Define a complicated (and probably useless) rehabilitation strategy
+## define a complicated (and probably useless) rehabilitation strategy
 mystrategy <- . %>%
   replace.n.highest.risk(n=2, failure.rate=f.rate) %>%
   replace.more.failures.than(max.failures=5) %>%
-  replace.older.than(max.age=100) %>%
-  replace.n.oldest(n=3) %>%
-  replace.n.random(n=4)
+  replace.older.than(max.age=70, max.cost=2e6)  %>%
+  replace.n.oldest(n=2) %>%
+  replace.n.random(n=2)
 ## This means: every year (if we have enough budget!), replace first the 2 pipes
 ## with the highest risk, then all pipes with more than 5 failures,
-## then all pipes older then 100 years, then the 3 oldest pipes remaining, and
-## finally replace 4 randomly selected pipes.
+## then all pipes older then 100 years (up to costs of 2e6), then the 3
+## oldest pipes remaining, and finally replace 4 randomly selected pipes.
 
 
-## 3) Run the simulation
+## run the simulation
 result <- simulate(t.sim=100,                  # run it for 100 years
                    expansion=0,                # do not expand the system
                    rehabilitation=mystrategy,  # use the strategy defined above
                    failure.rate=f.rate,        # use the failure rate defined above
                    income = 1e6,               # the annual income
                    initial.budget=30e6,
-                   initial.inventory=50)       # start the simulation with 50 new pipes
+                   initial.inventory=500)      # start the simulation with 50 new pipes
 
 str(result)                                    # a list of model states
 ```

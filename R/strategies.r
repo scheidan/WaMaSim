ea1477f08371113cc08382ab372bc996350c8fc0
rachelmarybarrett <rachelmarybarrett@gmail.com>
2017-04-12 18:20:35 +0200

    fix failure rate input also for replace.n.highest.risk function

--- R/strategies.r
+++ R/strategies.r
@@ -288,9 +288,9 @@ replace.n.highest.risk <- function(state, n, failure.rate, max.costs=Inf){
   for(i in which(inv$in.service)){
     
     Prob.fail <- failure.rate(age=state$time-inv$time.construction[i],
-                              inv$time.last.failure[i],
-                              inv$n.failure[i])
-    
+                              age.last.failure=state$time-inv$time.last.failure[i],
+                              n.failure=inv$n.failure[i])
+
     expected.failure.cost <- failure.cost(inv$diameter[i], mean=TRUE)
     
     risk[i] <- Prob.fail * expected.failure.cost
